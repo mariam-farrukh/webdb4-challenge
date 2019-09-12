ed5636871db083c37501869c1e6b076ec10c018f
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-09-12 16:51:35 -0500

    Migrations and Seeds Completed

--- data/seeds/04-instructions.js
+++ data/seeds/04-instructions.js
@@ -1,9 +1,13 @@
 exports.seed = function(knex) {
   return knex("instructions").insert([
-    {
-      step_number: 1,
-      instruction: "stir in bricks",
-      recipe_id: 1
-    }
+    {id: 1, step_number: 1, instruction: "step 1", recipe_id: 1},
+    {id: 2, step_number: 2, instruction: "step 2", recipe_id: 1},
+    {id: 3, step_number: 3, instruction: "step 3", recipe_id: 1},
+    {id: 4, step_number: 1, instruction: "start", recipe_id: 2},
+    {id: 5, step_number: 2, instruction: "make", recipe_id: 2},
+    {id: 6, step_number: 3, instruction: "done", recipe_id: 2},
+    {id: 7, step_number: 1, instruction: "gather", recipe_id: 3},
+    {id: 8, step_number: 2, instruction: "mix together", recipe_id: 3},
+    {id: 9, step_number: 3, instruction: "complete", recipe_id: 3}
   ]);
 };
